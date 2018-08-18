48f4e9389dc7d3e6c29c94f3fc56cd058afa72f5
IceHe.me <icehe.me@qq.com>
2018-08-18 15:57:39 +0800

    Add .config/zsh/ls.zsh

---
+++ .config/zsh/ls.zsh
@@ -0,0 +1,17 @@
+######
+# ls #
+######
+
+# List
+## Overwrite $ZSH/lib/directories.zsh
+alias l='ls -h'
+alias ll='ls -hl'
+alias la='ls -hA'
+alias lla='ls -hlA'
+
+# List & Grep
+alias lg='ls -h | grep'
+alias llg='ls -hl | grep'
+alias lag='ls -hA | grep'
+alias llag='ls -hlA | grep'
+
