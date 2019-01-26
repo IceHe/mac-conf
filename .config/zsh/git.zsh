6797dda83f4db41a384dbef0260bc6e89ee4f1c7
IceHe.xyz <icehe.xyz@qq.com>
2019-01-26 15:06:44 +0800

    Upd

--- .config/zsh/git.zsh
+++ .config/zsh/git.zsh
@@ -7,7 +7,7 @@ alias gcfl='git config -l'
 alias gcfe='git config -e'
 
 alias gcm='git commit -m'
-alias gu='git commit -m "Upd"'
+alias gu='git commit -m "."'
 alias gau='gaa && gu'
 
 alias gcom='git checkout master'
