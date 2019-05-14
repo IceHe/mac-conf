5d678de9afc5f4bcc8887b77a70798a1f14c4f10
IceHe <icehe.me@qq.com>
2019-05-15 02:09:10 +0800

    Update tmux.zsh

--- .config/zsh/tmux.zsh
+++ .config/zsh/tmux.zsh
@@ -8,6 +8,25 @@ alias tn='tmux new-session -s'
 alias tch="tmux split-window -h -c '#{pane_current_path}'"
 alias tcv="tmux split-window -v -c '#{pane_current_path}'"
 
+function tw {
+    cd ~/Document/Lib
+
+    # ICEHE
+    tmux new -s icehe -d
+
+    # lib
+    tmux rename-window -t 'icehe:1' lib
+    tmux send -t 'icehe:lib' 'cd ~/Documents/Lib' Enter 'git pull' Enter
+    tmux split-window -t 'icehe:lib' -c ~/Documents/Lib/_private
+    tmux send -t 'icehe:lib' 'git pull' Enter
+
+    # home
+    tmux neww -a -t icehe -n home -c ~
+    tmux send -t 'icehe:home' 'brew update && brew upgrade' Enter
+
+    tmux
+}
+
 # tmux restart window ( for new `.zshrc` config )
 function trw {
     tmux rename-window 'tmp_tmux_win'
