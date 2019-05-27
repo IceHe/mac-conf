74fc45e72116e51dbfcab85a5cb6fc0c791921f4
IceHe.xyz <icehe.xyz@qq.com>
2019-05-27 11:56:46 +0800

    Update proxy.zsh

--- .config/zsh/proxy.zsh
+++ .config/zsh/proxy.zsh
@@ -2,18 +2,21 @@
 # Proxy #
 #########
 
+function ss() {
+    #env | grep ALL_PROXY
+    env | grep "_proxy=" || echo "No proxy."
+}
+
 function sss() {
     #export ALL_PROXY=socks5://127.0.0.1:1081
     export http_proxy=http://127.0.0.1:1083;
     export https_proxy=http://127.0.0.1:1083;
+    ss
 }
 
 function uss() {
     #unset ALL_PROXY
     unset http_proxy;
     unset https_proxy;
+    ss
 }
-
-#alias ss='env | grep ALL_PROXY'
-alias ss='env | grep _proxy=' 
-
