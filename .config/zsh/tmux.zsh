904d56ce2124223881bc1e2e00f12c6656510e28
IceHe.xyz <icehe.me@qq.com>
2019-05-27 11:58:26 +0800

    Update tmux.zsh

--- .config/zsh/tmux.zsh
+++ .config/zsh/tmux.zsh
@@ -26,15 +26,3 @@ function tw {
 
     tmux
 }
-
-# tmux restart window ( for new `.zshrc` config )
-function trw {
-    tmux rename-window 'tmp_tmux_win'
-    tmp_tmux_prev_win_num=$(tmux display-message -p '#I')
-
-    tmux new-window -c "$(pwd)"
-    tmp_tmux_cur_win_num=$(tmux display-message -p '#I')
-
-    tmux swap-window -s $tmp_tmux_cur_win_num -t $tmp_tmux_prev_win_num
-    tmux kill-window -t 'tmp_tmux_win'
-}
