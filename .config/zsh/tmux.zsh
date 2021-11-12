a989dbfa446413fa7db3a52ca9674ebbac97e046
IceHe.xyz <icehe.xyz@qq.com>
2021-11-12 22:57:25 +0800

    2021-11-12 22:57:25 Fri

--- .config/zsh/tmux.zsh
+++ .config/zsh/tmux.zsh
@@ -18,6 +18,39 @@ function tw {
     tmux rename-window -t 'icehe:1' init
     tmux send -t 'icehe:lib' 'cd ~' Enter
 
+    # JS
+    tmux neww -a -t icehe -n js -c ~/Documents/js
+    tmux send -t 'icehe:js' 'git pull' Enter
+    tmux send -t 'icehe:js' 'clear' Enter
+    tmux split-window -t 'icehe:js' -c ~/Documents/js/packages/client
+    tmux send -t 'icehe:js' 'clear' Enter
+
+    ## JS / client
+    #tmux neww -a -t icehe -n client -c ~/Documents/js/packages/client
+    #tmux send -t 'icehe:client' 'clear' Enter
+
+    ## JS / playground
+    #tmux neww -a -t icehe -n playground -c ~/Documents/js/packages/playground
+    #tmux send -t 'icehe:playground' 'clear' Enter
+
+    ## JS / packages
+    #tmux neww -a -t icehe -n js/pkgs -c ~/Documents/js/packages/client
+    #tmux send -t 'icehe:js/pkgs' 'clear' Enter
+    #tmux split-window -t 'icehe:js/pkgs' -c ~/Documents/js/packages/playground
+    #tmux send -t 'icehe:js/pkgs' 'clear' Enter
+
+    # KOTLIN / android
+    tmux neww -a -t icehe -n kt/android -c ~/Documents/kotlin/android
+    tmux send -t 'icehe:kt/android' 'git pull' Enter
+    tmux send -t 'icehe:kt/android' 'clear' Enter
+
+    # LOGTO
+    tmux neww -a -t icehe -n logto -c ~/Documents/logto
+    tmux send -t 'icehe:logto' 'git pull' Enter
+    tmux send -t 'icehe:logto' 'clear' Enter
+    tmux split-window -t 'icehe:logto/core' -c ~/Documents/logto/packages/core
+    tmux send -t 'icehe:logto/core' 'clear' Enter
+
     # LIB
     tmux new-window -a -t icehe -n lib -c ~/Documents/lib
     tmux send -t 'icehe:lib' 'git pull' Enter
@@ -26,33 +59,6 @@ function tw {
     tmux split-window -t 'icehe:lib' -c ~/Documents/lib/_private
     tmux send -t 'icehe:lib' 'git pull' Enter
     tmux send -t 'icehe:lib' 'clear' Enter
-
-    # JS
-    tmux neww -a -t icehe -n js -c ~/Documents/js
-    tmux send -t 'icehe:js' 'git pull' Enter
-    tmux send -t 'icehe:js' 'clear' Enter
-
-    # JS / client
-    tmux neww -a -t icehe -n client -c ~/Documents/js/packages/client
-    tmux send -t 'icehe:client' 'clear' Enter
-
-    # JS / playground
-    tmux neww -a -t icehe -n playground -c ~/Documents/js/packages/playground
-    tmux send -t 'icehe:playground' 'clear' Enter
-
-    # KOTLIN / android
-    tmux neww -a -t icehe -n android -c ~/Documents/kotlin/android
-    tmux send -t 'icehe:android' 'git pull' Enter
-    tmux send -t 'icehe:android' 'clear' Enter
-
-    # LOGTO
-    tmux neww -a -t icehe -n logto -c ~/Documents/logto
-    tmux send -t 'icehe:logto' 'git pull' Enter
-    tmux send -t 'icehe:logto' 'clear' Enter
-
-    # LOGTO / core
-    tmux neww -a -t icehe -n core -c ~/Documents/logto/packages/core
-    tmux send -t 'icehe:core' 'clear' Enter
 
     # HOME
     tmux neww -a -t icehe -n home -c ~
