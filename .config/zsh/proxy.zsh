e9fd0536897db3f614a012e2fd262c45223a2789
IceHe.xyz <icehe.xyz@qq.com>
2021-12-07 00:01:24 +0800

    2021-12-07 00:01:24 Tue

--- .config/zsh/proxy.zsh
+++ .config/zsh/proxy.zsh
@@ -8,15 +8,15 @@ function ss() {
 }
 
 function sss() {
-    export all_proxy=socks5://127.0.0.1:7890
+    export all_proxy=socks5://127.0.0.1:7890;
     export http_proxy=http://127.0.0.1:7890;
     export https_proxy=http://127.0.0.1:7890;
-    ss
+    ss;
 }
 
 function uss() {
-    unset all_proxy
+    unset all_proxy;
     unset http_proxy;
     unset https_proxy;
-    ss
+    ss;
 }
