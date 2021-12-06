d3815391e312d3ca28e0b36b99a5089af8bb1434
IceHe.xyz <icehe.xyz@qq.com>
2021-12-06 23:54:01 +0800

    2021-12-06 23:54:01 Mon

---
+++ .init-local-git-config-file.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Initilize (Overwrite) the local Git configuration file
+echo '[commit]
+    # https://docs.github.com/en/authentication/managing-commit-signature-verification/signing-commits
+    gpgsign = false
+[core]
+	repositoryformatversion = 0
+	bare = false
+	logallrefupdates = true
+	ignorecase = true
+	precomposeunicode = true
+[submodule]
+	active = .
+[remote "origin"]
+	url = git@github.com:IceHe/mac-conf.git
+	fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "master"]
+	remote = origin
+	merge = refs/heads/master
+[user]
+    # https://git-scm.com/docs/git-config#Documentation/git-config.txt-username
+	name = IceHe.xyz
+	email = icehe.xyz@qq.com
+' > ./.git/config
+
