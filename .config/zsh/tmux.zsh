d7d402887ff8a4f4ef20cd037f0ff880e4945653
IceHe.xyz <icehe.xyz@qq.com>
2022-02-16 11:54:29 +0800

    2022-02-16 11:54:29 Wed

--- .config/zsh/tmux.zsh
+++ .config/zsh/tmux.zsh
@@ -7,76 +7,68 @@ alias t='tmux' alias tn='tmux new-session -s'
 alias tch="tmux split-window -h -c '#{pane_current_path}'"
 alias tcv="tmux split-window -v -c '#{pane_current_path}'"
 
+# Tmux Windows
 function tw {
+
+    # Prepare
+    
+    ## Pull latest IceHe's mac-conf
     echo -e "\n\$ cd ~ && git pull || return"
     cd ~ && git pull || return
+
+    ## Upgrade Oh-My-Zsh!
     echo -e "\n\$ .oh-my-zsh/tools/upgrade.sh"
     .oh-my-zsh/tools/upgrade.sh
 
-    cd ~/Projects/lib
+    ## Switch PWD to lib
+    # cd ~/Projects/lib
+    
+    ###########################################
 
-    # ICEHE
+    # Session ICEHE
     tmux new -s icehe -d
 
-    # init
+    # Window INIT 
     tmux rename-window -t 'icehe:1' init
     tmux send -t 'icehe:init' 'cd ~' Enter
 
     # JS : root & js
     tmux neww -a -t icehe -n js -c ~/Projects/js
-    tmux send -t 'icehe:js' 'git fetch -ap' Enter
-    tmux send -t 'icehe:js' 'git checkout master' Enter
-    tmux send -t 'icehe:js' 'git pull' Enter
-    tmux send -t 'icehe:js' 'git checkout -' Enter
-    tmux send -t 'icehe:js' 'git pull' Enter
-    # tmux send -t 'icehe:js' 'clear' Enter
+    tmux send -t 'icehe:js' 'git fetch -ap && echo && git checkout master && git pull && echo && git checkout - && git pull' Enter
     tmux split-window -t 'icehe:js' -c ~/Projects/js/packages/js
-    tmux send -t 'icehe:js' 'clear' Enter
 
-    ## JS : browser
+    # JS : browser
     tmux neww -a -t icehe -n browser -c ~/Projects/js/packages/browser
-    tmux send -t 'icehe:browser' 'clear' Enter
-    #tmux split-window -t 'icehe:browser' -c ~/Projects/js/packages/browser-sample
-    #tmux send -t 'icehe:browser' 'clear' Enter
+    # tmux split-window -t 'icehe:browser' -c ~/Projects/js/packages/browser-sample
 
-    ## JS : client & playground
-    #tmux neww -a -t icehe -n js/pkgs -c ~/Projects/js/packages/client
-    ##tmux send -t 'icehe:js/pkgs' 'clear' Enter
-    #tmux split-window -t 'icehe:js/pkgs' -c ~/Projects/js/packages/playground
-    #tmux send -t 'icehe:js/pkgs' 'clear' Enter
+    # JS : client & playground
+    # tmux neww -a -t icehe -n js/pkgs -c ~/Projects/js/packages/client
+    # tmux split-window -t 'icehe:js/pkgs' -c ~/Projects/js/packages/playground
 
     # KOTLIN : root
     tmux neww -a -t icehe -n kt -c ~/Projects/kotlin
-    tmux send -t 'icehe:kt' 'git pull' Enter
-    #tmux send -t 'icehe:kt' 'clear' Enter
+    tmux send -t 'icehe:kt' 'git fetch -ap && echo && git pull' Enter
 
     # SWIFT : swift
     tmux neww -a -t icehe -n swift -c ~/Projects/swift
-    tmux send -t 'icehe:swift' 'git pull' Enter
-    #tmux send -t 'icehe:swift' 'clear' Enter
+    tmux send -t 'icehe:swift' 'git fetch -ap && echo && git pull' Enter
 
     # LOGTO : root & core
     tmux neww -a -t icehe -n logto -c ~/Projects/logto
-    tmux send -t 'icehe:logto' 'git pull' Enter
-    #tmux send -t 'icehe:logto' 'clear' Enter
+    tmux send -t 'icehe:logto' 'git fetch -ap && echo && git checkout master && git pull && echo && git checkout - && git pull' Enter
     tmux split-window -t 'icehe:logto' -c ~/Projects/logto/packages/core
-    tmux send -t 'icehe:logto' 'clear' Enter
 
     # LIB
     tmux new-window -a -t icehe -n lib -c ~/Projects/lib
     tmux send -t 'icehe:lib' 'git pull' Enter
     tmux send -t 'icehe:lib' 'nohup npx docsify serve -p 3100 . &' Enter
-    #tmux send -t 'icehe:lib' 'clear' Enter
     tmux split-window -t 'icehe:lib' -c ~/Projects/lib/_private
     tmux send -t 'icehe:lib' 'git pull' Enter
-    #tmux send -t 'icehe:lib' 'clear' Enter
 
     # HOME
     tmux neww -a -t icehe -n home -c ~
     tmux send -t 'icehe:home' 'brew update && brew upgrade' Enter
-    #tmux send -t 'icehe:home' 'clear' Enter
     tmux split-window -t 'icehe:home' -c ~
-    tmux send -t 'icehe:home' 'clear' Enter
 
     # clean up
     tmux send -t 'icehe:init' 'exit' Enter
