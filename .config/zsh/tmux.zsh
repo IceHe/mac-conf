998de23e3de9a68f102645cf870d6fa7bacd92b7
IceHe.xyz <icehe.xyz@qq.com>
2022-08-09 14:43:08 +0800

    2022-08-09 14:43:08 Tue

--- .config/zsh/tmux.zsh
+++ .config/zsh/tmux.zsh
@@ -29,10 +29,10 @@ function tw {
     
     ###########################################
 
-    # Session ICEHE
+    # Session icehe
     tmux new -s icehe -d
 
-    # Window INIT 
+    # Window init 
     tmux rename-window -t 'icehe:1' init
     tmux send -t 'icehe:init' 'cd ~' Enter
 
@@ -43,50 +43,47 @@ function tw {
     # core - logto
     tmux neww -a -t icehe -n core -c ~/Projects/logto/packages/core
 
-    # integration-tests - logto
-    tmux neww -a -t icehe -n int-test -c ~/Projects/logto/packages/integration-tests
+    # # int-tests - logto
+    # tmux neww -a -t icehe -n int-test -c ~/Projects/logto/packages/integration-tests
+   
+    # # schemas - logto
+    # tmux neww -a -t icehe -n schemas -c ~/Projects/logto/packages/schemas
 
-    ## docs - logto
-    #tmux neww -a -t icehe -n docs -c ~/Projects/docs
-    #tmux send -t 'icehe:docs' 'gujp' Enter
-    #tmux split-window -t 'icehe:docs' -c ~/Projects/docs
+    # # docs
+    # tmux neww -a -t icehe -n docs -c ~/Projects/docs
+    # tmux send -t 'icehe:docs' 'gujp' Enter
+    # tmux split-window -t 'icehe:docs' -c ~/Projects/docs
 
-    ## fb - LOGTO 
-    #tmux neww -a -t icehe -n fb -c ~/Projects/logto/packages/connector-facebook
+    # # root & browser-sample - js
+    # tmux neww -a -t icehe -n js -c ~/Projects/js
+    # tmux send -t 'icehe:js' 'gujp' Enter
+    # tmux split-window -t 'icehe:js' -c ~/Projects/js/packages/browser-sample
 
-    ## schemas - logto
-    #tmux neww -a -t icehe -n schemas -c ~/Projects/logto/packages/schemas
-
-    # JS : root & browser-sample
-    tmux neww -a -t icehe -n js -c ~/Projects/js
-    tmux send -t 'icehe:js' 'gujp' Enter
-    tmux split-window -t 'icehe:js' -c ~/Projects/js/packages/browser-sample
-
-    # # JS : browser ( & js )
+    # # browser - js
     # tmux neww -a -t icehe -n browser -c ~/Projects/js/packages/js
     # tmux split-window -t 'icehe:browser' -c ~/Projects/js/packages/browser
 
-    # # KOTLIN : root
-    # tmux neww -a -t icehe -n kt -c ~/Projects/kotlin
-    # tmux send -t 'icehe:kt' 'git fetch -ap && echo && git pull' Enter
+    # root - kotlin
+    tmux neww -a -t icehe -n kt -c ~/Projects/kotlin
+    tmux send -t 'icehe:kt' 'git fetch -ap && echo && git pull' Enter
 
-    # # SWIFT : swift
+    # # root - swift
     # tmux neww -a -t icehe -n swift -c ~/Projects/swift
     # tmux send -t 'icehe:swift' 'git fetch -ap && echo && git pull' Enter
 
-    # LIB
+    # Lib
     tmux new-window -a -t icehe -n lib -c ~/Projects/lib
     tmux send -t 'icehe:lib' 'git add --all && git stash && echo && git pull && git stash pop' Enter
     tmux send -t 'icehe:lib' 'nohup npx docsify serve -p 3100 . &' Enter
     tmux split-window -t 'icehe:lib' -c ~/Projects/lib/_private
     tmux send -t 'icehe:lib' 'git pull' Enter
 
-    # HOME
+    # Home
     tmux neww -a -t icehe -n home -c ~
     tmux send -t 'icehe:home' 'brew update && brew upgrade' Enter
     tmux split-window -t 'icehe:home' -c ~
 
-    # clean up
+    # Clean up
     tmux send -t 'icehe:init' 'exit' Enter
     tmux select-window -t 'icehe:js'
 
