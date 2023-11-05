5b2e3cade7f079caddff52cd1c6d1ce3e22171eb
IceHe.xyz <icehe.xyz@qq.com>
2023-11-06 01:12:04 +0800

    .

--- .config/zsh/proxy.zsh
+++ .config/zsh/proxy.zsh
@@ -8,9 +8,12 @@ function ss() {
 }
 
 function sss() {
-    export all_proxy=socks5://127.0.0.1:7890;
-    export http_proxy=http://127.0.0.1:7890;
-    export https_proxy=http://127.0.0.1:7890;
+    #export all_proxy=socks5://127.0.0.1:7890;
+    #export http_proxy=http://127.0.0.1:7890;
+    #export https_proxy=http://127.0.0.1:7890;
+    export all_proxy=socks5://127.0.0.1:15235;
+    export http_proxy=http://127.0.0.1:15236;
+    export https_proxy=http://127.0.0.1:15236;
     ss;
 }
 
